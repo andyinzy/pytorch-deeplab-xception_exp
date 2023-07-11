238204832c23eff1fa0992a0fc0d63099f71e51c
ziyuanyin <ziyuan_yin@foxmail.com>
2023-07-11 22:44:51 +0800

    submit 071101

--- modeling/backbone/xception.py
+++ modeling/backbone/xception.py
@@ -14,11 +14,11 @@ def fixed_padding(inputs, kernel_size, dilation):
     padded_inputs = F.pad(inputs, (pad_beg, pad_end, pad_beg, pad_end)) # 使用F.pad函数对输入进行填充，填充的大小为(pad_beg, pad_end, pad_beg, pad_end)。
     return padded_inputs
 
-
+# 深度可分离卷积
 class SeparableConv2d(nn.Module):
     def __init__(self, inplanes, planes, kernel_size=3, stride=1, dilation=1, bias=False, BatchNorm=None):
         super(SeparableConv2d, self).__init__()
-        # 在初始化函数中，定义了三个卷积层：conv1是一个普通的卷积层，bn是一个批归一化层，pointwise是一个1x1的卷积层。
+        # 在初始化函数中，定义了三个卷积层：conv1是一个depthwise卷积层，bn是一个批归一化层，pointwise卷积层。
         self.conv1 = nn.Conv2d(inplanes, inplanes, kernel_size, stride, 0, dilation,
                                groups=inplanes, bias=bias)
         self.bn = BatchNorm(inplanes)
@@ -33,57 +33,73 @@ class SeparableConv2d(nn.Module):
 
 
 class Block(nn.Module):
+    # (输入通道，输出通道，同种conv数量，步长，空洞率，是否在从relu开始，是否为最后（20）模块)
     def __init__(self, inplanes, planes, reps, stride=1, dilation=1, BatchNorm=None,
                  start_with_relu=True, grow_first=True, is_last=False):
         super(Block, self).__init__()
         # 如果planes不等于inplanes或者stride不等于1，则需要进行跳跃连接，使用一个1x1的卷积层和批归一化层来调整维度。
+        # 区别block1-3与block4-19
+        # 满足条件为block1-3，定义1X1卷积和卷积后的bn层
         if planes != inplanes or stride != 1:
             self.skip = nn.Conv2d(inplanes, planes, 1, stride=stride, bias=False)
-            self.skipbn = BatchNorm(planes)
-        else:
+            self.skipbn = BatchNorm(planes) # batch_normal
+        else:   # 不满足条件为block4-19，不定义skip
             self.skip = None
-
+        # 定义relu层
+        # inplace = True ,会改变输入数据的值,节省反复申请与释放内存的空间与时间,只是将原来的地址传递,效率更好
         self.relu = nn.ReLU(inplace=True)
         rep = []    # 空的列表rep，用于存储残差块的层。
 
-        filters = inplanes
+        filters = inplanes  # 记录输入通道数
         # 根据grow_first参数的值，决定是先增加通道数还是先减少通道数
         # 为True，则先增加通道数，即先经过一个ReLU激活函数、一个可分离卷积层和一个批归一化层，然后将filters更新为planes
         # 为False，则先减少通道数，即先经过一个ReLU激活函数、一个可分离卷积层和一个批归一化层，然后将filters更新为inplanes。
+        # 如果是每个组开始，则先定义一个由relu-sepconv-bn组成的可分离卷积块
         if grow_first:
-            rep.append(self.relu)
-            rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))
-            rep.append(BatchNorm(planes))
-            filters = planes
-        # 根据reps参数的值，循环添加reps - 1个残差块。每个残差块都包括一个ReLU激活函数、一个可分离卷积层和一个批归一化层。
+            rep.append(self.relu)     # 第一层relu
+            rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))  # 第二层3X3可分离卷积层
+            rep.append(BatchNorm(planes))   #第三层bn层
+            filters = planes  # filters更新为输出通道数
+
+        # 定义rsp-1个由relu-sepconv-bn组成的可分离卷积块，每个残差块都包括一个ReLU激活函数、一个深度可分离卷积层和一个批归一化层。
+        # 同上，第一层relu，第二层3X3可分离卷积层，第三层bn层
         for i in range(reps - 1):
             rep.append(self.relu)
             rep.append(SeparableConv2d(filters, filters, 3, 1, dilation, BatchNorm=BatchNorm))
             rep.append(BatchNorm(filters))
 
+        # 如果不是每个组开始，定义一个由relu-sepconv-bn组成的可分离卷积块
+        # 同上，第一层relu，第二层3X3可分离卷积层，第三层bn层
         if not grow_first:
             rep.append(self.relu)
             rep.append(SeparableConv2d(inplanes, planes, 3, 1, dilation, BatchNorm=BatchNorm))
             rep.append(BatchNorm(planes))
+
         # 如果stride等于1且is_last为True，则添加一个ReLU激活函数、一个可分离卷积层和一个批归一化层，用于最后的卷积操作
+        # 判断是否是block1-3
         if stride != 1:
             rep.append(self.relu)
-            rep.append(SeparableConv2d(planes, planes, 3, 2, BatchNorm=BatchNorm))
+            rep.append(SeparableConv2d(planes, planes, 3, 2, BatchNorm=BatchNorm))  # 定义3X3下采样可分离卷积层
             rep.append(BatchNorm(planes))
 
+        # 改写原来的stride=1为=2
         if stride == 1 and is_last:
             rep.append(self.relu)
             rep.append(SeparableConv2d(planes, planes, 3, 1, BatchNorm=BatchNorm))
             rep.append(BatchNorm(planes))
         # 如果start_with_relu为False，则将列表rep的第一个元素移除。
+        # 是否保留块开始的RELU（）
         if not start_with_relu:
             rep = rep[1:]
         # 使用nn.Sequential将列表rep中的层组合成一个序列。
         self.rep = nn.Sequential(*rep)
 
     def forward(self, inp):
+        # 输入值放入rep中
         x = self.rep(inp)
 
+        # 判断是否产生skip（逻辑结构为32-41行）
+        # 如果产生，证明在block1-3中，这时候需要把skip和bn实例化出来
         if self.skip is not None:
             skip = self.skip(inp)
             skip = self.skipbn(skip)
@@ -104,9 +120,9 @@ class AlignedXception(nn.Module):
         super(AlignedXception, self).__init__()
 
         if output_stride == 16:
-            entry_block3_stride = 2
-            middle_block_dilation = 1
-            exit_block_dilations = (1, 2)
+            entry_block3_stride = 2 # block1-3中最下层步长
+            middle_block_dilation = 1  # block4-19中空洞率
+            exit_block_dilations = (1, 2)  # block20中空洞率
         elif output_stride == 8:
             entry_block3_stride = 1
             middle_block_dilation = 2
@@ -116,6 +132,7 @@ class AlignedXception(nn.Module):
 
 
         # Entry flow
+        # # block1-3, inplanes = 3
         self.conv1 = nn.Conv2d(3, 32, 3, stride=2, padding=1, bias=False)
         self.bn1 = BatchNorm(32)
         self.relu = nn.ReLU(inplace=True)
@@ -130,6 +147,7 @@ class AlignedXception(nn.Module):
                             start_with_relu=True, grow_first=True, is_last=True)
 
         # Middle flow
+        # block4-19
         self.block4  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
                              BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
         self.block5  = Block(728, 728, reps=3, stride=1, dilation=middle_block_dilation,
@@ -164,6 +182,7 @@ class AlignedXception(nn.Module):
                              BatchNorm=BatchNorm, start_with_relu=True, grow_first=True)
 
         # Exit flow
+        # block20
         self.block20 = Block(728, 1024, reps=2, stride=1, dilation=exit_block_dilations[0],
                              BatchNorm=BatchNorm, start_with_relu=True, grow_first=False, is_last=True)
 
@@ -196,7 +215,7 @@ class AlignedXception(nn.Module):
         x = self.block1(x)
         # add relu here
         x = self.relu(x)
-        low_level_feat = x
+        low_level_feat = x  # 四分之一大小
         x = self.block2(x)
         x = self.block3(x)
 
